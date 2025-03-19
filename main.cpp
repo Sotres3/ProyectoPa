c5e07884fdb295f1e060176a6ddd9297cfc9782a
eduardosotreszerecero <0270864@up.edu.mx>
2025-03-19 11:55:37 -0600

    Prueba menu

--- main.cpp
+++ main.cpp
@@ -1,17 +1,39 @@
 #include <iostream>
 using namespace std;
 
-int main(){
-    int opc;
 
+void seleccionarBarcos();
+void posicionarBarcos();
+void iniciarPartida();
+void salir();
+
+
+int main() {
+    int opc;
     do {
-        cout << "\n--- Menu Principal ---\n";
+        cout << "\n    Menu Principal    \n";
         cout << "1. Seleccionar barcos\n";
         cout << "2. Posicionar barcos\n";
         cout << "3. Iniciar partida\n";
-        cout << "4. Mostrar tablero\n";
-        cout << "5. Salir\n";
+        cout << "4. Salir\n";
         cout << "Seleccione una opcion: ";
         cin >> opc;
-    }
+        switch (opc) {
+            case 1:
+                seleccionarBarcos();
+                break;
+            case 2:
+                posicionarBarcos();
+                break;
+            case 3:
+                iniciarPartida();
+                break;
+            case 4:
+                salir();
+                break;
+            default:
+                cout << "Opcion no valida.\n";
+        };
+
+    } while (opc != 5);
 };
