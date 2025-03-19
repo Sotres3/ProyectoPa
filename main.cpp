360b1d9fd70805dd7633df5160e50d07beb2b854
eduardosotreszerecero <0270864@up.edu.mx>
2025-03-19 11:49:54 -0600

    Prueba menu

--- main.cpp
+++ main.cpp
@@ -1,6 +1,17 @@
-
-
+#include <iostream>
+using namespace std;
 
 int main(){
+    int opc;
 
-}
+    do {
+        cout << "\n--- Menu Principal ---\n";
+        cout << "1. Seleccionar barcos\n";
+        cout << "2. Posicionar barcos\n";
+        cout << "3. Iniciar partida\n";
+        cout << "4. Mostrar tablero\n";
+        cout << "5. Salir\n";
+        cout << "Seleccione una opcion: ";
+        cin >> opc;
+    }
+};
